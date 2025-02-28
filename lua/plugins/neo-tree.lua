348a87de150d8af37be17333b45eb1b6d62e201d
oneqit <77319843+oneqit@users.noreply.github.com>
2025-02-28 19:22:03 +0900

    reveal hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,12 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
 }
