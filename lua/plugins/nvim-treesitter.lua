18348e33848b6a137d819687fa9886032fdeb14a
oneqit <77319843+oneqit@users.noreply.github.com>
2025-03-19 14:07:29 +0900

    setting up plugins

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "lua", "vimdoc", "python", "markdown", "markdown_inline", "yaml" },
+      ensure_installed = { "lua", "vimdoc", "python", "markdown", "markdown_inline", "yaml", "kotlin" },
       highlight = { enable = true },
       indent = { enable = true },
     })
