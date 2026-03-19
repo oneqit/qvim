accfbba66ed1898cafbadbb8eb9286ccb8041563
oneqit <77319843+oneqit@users.noreply.github.com>
2026-03-19 12:56:25 +0900

    fix: integrate custom buffer close handler into bufferline
    - Add close_buffer function to create empty buffer when closing last one in lua/plugins/bufferline.lua
    - Apply handler to close_command and right_mouse_command options
    - Uses Snacks.bufdelete for normal cases, manual buffer creation for last buffer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,8 +7,27 @@ return {
   },
   keys = {},
   config = function()
+    -- Snacks.bufdelete 사용 (마지막 버퍼일 때 빈 버퍼 생성)
+    local function close_buffer(bufnum)
+      local buf = bufnum or vim.api.nvim_get_current_buf()
+      local listed = vim.tbl_filter(function(b)
+        return vim.bo[b].buflisted
+      end, vim.api.nvim_list_bufs())
+      if #listed > 1 then
+        Snacks.bufdelete({ buf = buf })
+      else
+        local new_buf = vim.api.nvim_create_buf(true, false)
+        vim.api.nvim_win_set_buf(0, new_buf)
+        if vim.api.nvim_buf_is_valid(buf) then
+          vim.api.nvim_buf_delete(buf, {})
+        end
+      end
+    end
+
     require("bufferline").setup({
       options = {
+        close_command = close_buffer,
+        right_mouse_command = close_buffer,
         diagnostics = "nvim_lsp",
         always_show_bufferline = true,
         offsets = {
