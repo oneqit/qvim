4ab79cef1e282ac8c0cac9464d44499cf820047c
oneqit <77319843+oneqit@users.noreply.github.com>
2026-03-25 10:06:00 +0900

    feat: improve editor defaults for markdown and autopairs
    - Add markdown to 2-space tabstop filetype list in lua/config/autocmds.lua
    - Configure nvim-autopairs to skip pairing when cursor is next to non-whitespace in lua/plugins/nvim-autopairs.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -27,7 +27,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd("FileType", {
   group = "FileTypeTabStop",
-  pattern = { "lua", "javascript", "typescript", "json" },
+  pattern = { "lua", "javascript", "typescript", "json", "markdown" },
   callback = function()
     vim.bo.tabstop = 2
     vim.bo.shiftwidth = 2
